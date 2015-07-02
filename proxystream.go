00ab71916e5d07c72d0e604499d146992eba88e8
Ewetumo Alexander <trinoxf@gmail.com>
2015-07-02 15:12:09 +0100

    added https and http proxy sketches

--- proxystream.go
+++ proxystream.go
@@ -2,10 +2,13 @@ package servicedrop
 
 import (
 	"bufio"
+	"crypto/tls"
+	"crypto/x509"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 
 	"github.com/influx6/flux"
@@ -39,7 +42,7 @@ type (
 		ErrNotify() NotifierError
 		CloseNotify() Notifier
 		//implement in lower-class/subcomposition of stream
-		DoBroker(io.Writer, io.Reader, Notifier)
+		DoBroker(io.WriteCloser, io.ReadCloser, Notifier)
 		SrcReader() io.ReadCloser
 		SrcWriter() io.WriteCloser
 		DestReader() io.ReadCloser
@@ -48,7 +51,7 @@ type (
 		CloseSrc() error
 		// BufferedSrc() *bufio.ReadWriter
 		// BufferedDest() *bufio.ReadWriter
-		// handleOperation()
+		handleOperation()
 	}
 
 	//TCPProxyStream handles bare tcp stream proxying
@@ -60,58 +63,35 @@ type (
 
 	//HTTPProxyStream provides http based proxying
 	HTTPProxyStream struct {
-		*ProxyStream
+		*TCPProxyStream
 	}
 
 	//HTTPSProxyStream provides https based proxying
 	HTTPSProxyStream struct {
-		*ProxyStream
+		*TCPProxyStream
+	}
+
+	//Nopwriter provides a writer with a Close member func
+	Nopwriter struct {
+		io.Writer
 	}
 )
-
-//handle process handles the operation of the streams
-func handleProcess(p ProxyStreams) {
-	rdest := p.DestReader()
-	wdest := p.DestWriter()
-
-	rsrc := p.SrcReader()
-	wsrc := p.SrcWriter()
-
-	destwriter := io.MultiWriter(wdest, p.Out())
-	srcwriter := io.MultiWriter(wsrc, p.In())
-
-	go p.DoBroker(destwriter, rsrc, p.SrcNotify())
-	go p.DoBroker(srcwriter, rdest, p.DestNotify())
-
-	go reportError(p.ErrNotify())
-
-	select {
-	case <-p.CloseNotify():
-		log.Println("Closing destination first streams!")
-		ex := p.CloseDest()
-		if ex != nil {
-			go func() { p.ErrNotify() <- ex }()
-		}
-	case <-p.SrcNotify():
-		//close the server and notifier serverend
-		ex := p.CloseDest()
-		if ex != nil {
-			go func() { p.ErrNotify() <- ex }()
-		}
-	case <-p.DestNotify():
-		//close the client and notifier clientend
-		ex := p.CloseSrc()
-		if ex != nil {
-			go func() { p.ErrNotify() <- ex }()
-		}
-	}
-}
 
 //reportEror helper to just log out err from a error channel
 func reportError(n NotifierError) {
 	for ex := range n {
 		log.Printf("Recieved Error %+s", ex.Error())
 	}
+}
+
+//Close returns nil
+func (n *Nopwriter) Close() error {
+	return nil
+}
+
+//NopWriter returns a new nopwriter instance
+func NopWriter(w io.Writer) *Nopwriter {
+	return &Nopwriter{w}
 }
 
 //NewProxyStream returns a new proxy streamer
@@ -167,6 +147,32 @@ func (p *ProxyStream) In() flux.StackStreamers {
 	return p.incoming
 }
 
+//handleOperation can be used to perform some work
+func (p *ProxyStream) handleOperation() {
+	go reportError(p.ErrNotify())
+
+	select {
+	case <-p.CloseNotify():
+		log.Println("Closing destination first streams!")
+		ex := p.CloseDest()
+		if ex != nil {
+			go func() { p.ErrNotify() <- ex }()
+		}
+	case <-p.SrcNotify():
+		//close the server and notifier serverend
+		ex := p.CloseDest()
+		if ex != nil {
+			go func() { p.ErrNotify() <- ex }()
+		}
+	case <-p.DestNotify():
+		//close the client and notifier clientend
+		ex := p.CloseSrc()
+		if ex != nil {
+			go func() { p.ErrNotify() <- ex }()
+		}
+	}
+}
+
 //CloseDest closes the destination
 func (p *ProxyStream) CloseDest() error {
 	return nil
@@ -209,14 +215,19 @@ func (p *ProxyStream) Close() error {
 	return nil
 }
 
-//TCPStream provides a tcp proxy streamer
-func TCPStream(src net.Conn, dest net.Conn, in, out flux.StackStreamers) ProxyStreams {
-	ts := &TCPProxyStream{
+//NewTCPProxyStream provides a tcp proxy streamer
+func NewTCPProxyStream(src net.Conn, dest net.Conn, in, out flux.StackStreamers) *TCPProxyStream {
+	return &TCPProxyStream{
 		ProxyStream: NewProxyStream(in, out),
 		src:         src,
 		dest:        dest,
 	}
-	go handleProcess(ts)
+}
+
+//TCPStream provides a tcp proxy streamer
+func TCPStream(src net.Conn, dest net.Conn, in, out flux.StackStreamers) ProxyStreams {
+	ts := NewTCPProxyStream(src, dest, in, out)
+	go ts.handleProcess()
 	return ts
 }
 
@@ -224,6 +235,22 @@ func TCPStream(src net.Conn, dest net.Conn, in, out flux.StackStreamers) ProxySt
 func (p *TCPProxyStream) CloseDest() error {
 	p.DestNotify() <- struct{}{}
 	return p.dest.Close()
+}
+
+//handleProcess process handles the operation of the streams
+func (p *TCPProxyStream) handleProcess() {
+	rdest := p.DestReader()
+	wdest := p.DestWriter()
+
+	rsrc := p.SrcReader()
+	wsrc := p.SrcWriter()
+
+	destwriter := NopWriter(io.MultiWriter(wdest, p.Out()))
+	srcwriter := NopWriter(io.MultiWriter(wsrc, p.In()))
+
+	go p.DoBroker(destwriter, rsrc, p.SrcNotify())
+	go p.DoBroker(srcwriter, rdest, p.DestNotify())
+	go p.handleOperation()
 }
 
 //CloseSrc closes the destination
@@ -253,7 +280,7 @@ func (p *TCPProxyStream) SrcReader() io.ReadCloser {
 }
 
 //DoBroker provides the internal opeations of the streamer
-func (p *TCPProxyStream) DoBroker(dest io.Writer, src io.Reader, end Notifier) {
+func (p *TCPProxyStream) DoBroker(dest io.WriteCloser, src io.ReadCloser, end Notifier) {
 	_, ex := io.Copy(dest, src)
 
 	if ex != nil {
@@ -263,24 +290,180 @@ func (p *TCPProxyStream) DoBroker(dest io.Writer, src io.Reader, end Notifier) {
 	end <- struct{}{}
 }
 
-//DoBroker provides the internal opeations of the streamer
-func (p *HTTPSProxyStream) DoBroker(dest io.Writer, src io.Reader, end Notifier) {
-	// _, ex := io.Copy(dest, src)
-	//
-	// if ex != nil {
-	// 	go func() { p.errorend <- ex }()
-	// }
-	//
-	// end <- struct{}{}
+//handleProcess process handles the operation of the streams
+func (p *HTTPSProxyStream) handleProcess() {
+	rdest := p.DestReader()
+	wdest := p.DestWriter()
+
+	rsrc := p.SrcReader()
+	wsrc := p.SrcWriter()
+
+	destwriter := NopWriter(io.MultiWriter(wdest, p.Out()))
+	srcwriter := NopWriter(io.MultiWriter(wsrc, p.In()))
+
+	defer p.DoBroker(destwriter, rsrc, p.SrcNotify())
+	defer p.DoBroker(srcwriter, rdest, p.DestNotify())
+	defer p.handleOperation()
+
+	tlscon, ok := p.src.(*tls.Conn)
+
+	if !ok {
+		log.Println("HTTPSProxy: Could not type assert tls.Conn!")
+		return
+	}
+
+	log.Println("HTTPSProxy: We received tls assertion success!")
+	err := tlscon.HandShake()
+
+	if err != nil {
+		log.Printf("HTTPSProxy: HandShake failed (%+s)!", err)
+		return
+	}
+
+	log.Printf("HTTPSProxy: HandShake Successful!")
+	state := tlscon.ConnectionState()
+
+	log.Println("HTTPSProxy: Reaching for ServerKeys")
+
+	var clientcert *x509.Certificate
+
+	for _, v := range state.PeerCertificates {
+		log.Printf("%#+v", v.Subject.CommonName)
+		log.Printf("%#+v", v.Subject.SerialNumber)
+		clientcert = v
+	}
+
+	reader := bufio.NewReader(tls)
+	req, err := http.ReadRequest(reader)
+
+	if err != nil {
+		log.Printf("HTTPSProxy: ReadRequest failed (%+s)!", err)
+		return
+	}
+
+	if req == nil {
+		log.Printf("HTTPSProxy: Received No Request (%+s)!", err)
+		return
+	}
+
+	req.Write(destwriter)
+
+	desttls, ok := p.dest.(*tls.Conn)
+
+	if !ok {
+		log.Printf("HTTPSProxy: Incorrect tls.Conn assert for destination!")
+		return
+	}
+
+	resread := bufio.NewReader(desttls)
+	res, err := http.ReadResponse(resread)
+
+	if err != nil {
+		log.Printf("HTTPSProxy: ReadResponse failed (%+s)!", err)
+		return
+	}
+
+	if res == nil {
+		log.Printf("HTTPSProxy: NoResponse received!")
+		return
+	}
+
+	res.Write(srcwriter)
+
+	//remove token cookie
+	log.Println("HTTPSProxy: Finalized Proxy Request")
 }
 
 //DoBroker provides the internal opeations of the streamer
-func (p *HTTPProxyStream) DoBroker(dest io.Writer, src io.Reader, end Notifier) {
-	// _, ex := io.Copy(dest, src)
+func (p *HTTPSProxyStream) DoBroker(dest io.WriteCloser, src io.ReadCloser, end Notifier) {
+	end <- struct{}{}
+}
+
+//DoBroker provides the internal opeations of the streamer
+func (p *HTTPProxyStream) DoBroker(dest io.WriteCloser, src io.ReadCloser, end Notifier) {
+	end <- struct{}{}
+}
+
+//handleProcess process handles the operation of the streams
+func (p *HTTPProxyStream) handleProcess() {
+	rdest := p.DestReader()
+	wdest := p.DestWriter()
+
+	rsrc := p.SrcReader()
+	wsrc := p.SrcWriter()
+
+	destwriter := NopWriter(io.MultiWriter(wdest, p.Out()))
+	srcwriter := NopWriter(io.MultiWriter(wsrc, p.In()))
+
+	defer p.DoBroker(destwriter, rsrc, p.SrcNotify())
+	defer p.DoBroker(srcwriter, rdest, p.DestNotify())
+	defer p.handleOperation()
+
+	// tls, ok := p.src.(*http.Conn)
 	//
-	// if ex != nil {
-	// 	go func() { p.errorend <- ex }()
+	// if !ok {
+	// 	log.Println("HTTPSProxy: Could not type assert http.Conn!")
+	// 	return
+	// }
+
+	// log.Println("HTTPSProxy: We received tls assertion success!")
+	// err := tlscon.HandShake()
+	//
+	// if err != nil {
+	// 	log.Printf("HTTPSProxy: HandShake failed (%+s)!", err)
+	// 	return
 	// }
 	//
-	// end <- struct{}{}
+	// log.Printf("HTTPSProxy: HandShake Successful!")
+	// state := tlscon.ConnectionState()
+	//
+	// log.Println("HTTPSProxy: Reaching for ServerKeys")
+	//
+	// var clientcert *x509.Certificate = nil
+	//
+	// for _, v := range state.PeerCertificates {
+	// 	log.Printf("%#+v", v.Subject.CommonName)
+	// 	log.Printf("%#+v", v.Subject.SerialNumber)
+	// 	clientcert = v
+	// }
+
+	reader := bufio.NewReader(p.src)
+	req, err := http.ReadRequest(reader)
+
+	if err != nil {
+		log.Printf("HTTPSProxy: ReadRequest failed (%+s)!", err)
+		return
+	}
+
+	if req == nil {
+		log.Printf("HTTPSProxy: Received No Request (%+s)!", err)
+		return
+	}
+
+	req.Write(destwriter)
+
+	// desttls, ok := p.dest.(net.Conn)
+	//
+	// if !ok {
+	// 	log.Printf("HTTPSProxy: Incorrect tls.Conn assert for destination!")
+	// 	return
+	// }
+
+	resread := bufio.NewReader(p.dest)
+	res, err := http.ReadResponse(resread)
+
+	if err != nil {
+		log.Printf("HTTPSProxy: ReadResponse failed (%+s)!", err)
+		return
+	}
+
+	if res == nil {
+		log.Printf("HTTPSProxy: NoResponse received!")
+		return
+	}
+
+	res.Write(srcwriter)
+
+	//remove token cookie
+	log.Println("HTTPSProxy: Finalized Proxy Request")
 }
